e21bc9f721600c04d00047ebd3650625fbe5bb68
Michael Mugnai <michael.mugnai@gmail.com>
2024-06-28 19:44:51 +0200

    move in script path before copying

--- install_libs.sh
+++ install_libs.sh
@@ -2,6 +2,9 @@
 
 libs=("libViconDataStreamSDK_CPP.so" "libboost_system-mt-x64.so.1.75.0" "libboost_thread-mt-x64.so.1.75.0" "libboost_timer-mt-x64.so.1.75.0" "libboost_chrono-mt-x64.so.1.75.0")
 
+script_path=$(dirname $(realpath $0))
+cd "$script_path"
+
 echo "Installing shared libraries, please wait"
 for lib in "${libs[@]}"; do
     # echo "Copying $lib"
